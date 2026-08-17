01befa3f1ed7ff2b4a0a22203101951347e3bc83
Sam Coulthurst <scoulthurst2012@gmail.com>
2026-08-17 17:43:37 +0100

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def strToHash(self, s):
+        hash = {}
+        for letter in s:
+            hash[letter] = hash.get(letter,0) + 1
+
+        return hash
+
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        else:
+            biggest = s2
+            smallest = s1
+        len_smallest = len(smallest)
+        len_biggest = len(biggest)
+        
+        for i in range(len_biggest-len_smallest+1):
+            window = biggest[i:i+len_smallest]
+            print(i, window)
+            if self.strToHash(window) == self.strToHash(smallest):
+                return True
+        return False
